993faf593d610dcb7ad51dc4549b993e4ca60463
zhangyunkai <zhangyunkai@hyjf.com>
2020-03-17 16:41:27 +0800

    project init

---
+++ ruoyi-common/src/main/java/com/ruoyi/common/enums/DataSourceType.java
@@ -0,0 +1,19 @@
+package com.ruoyi.common.enums;
+
+/**
+ * 数据源
+ * 
+ * @author framework
+ */
+public enum DataSourceType
+{
+    /**
+     * 主库
+     */
+    MASTER,
+
+    /**
+     * 从库
+     */
+    SLAVE
+}
