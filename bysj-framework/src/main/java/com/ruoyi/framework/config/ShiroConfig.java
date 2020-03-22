dcc631ff7ce457eea31a334eb899288b31061963
zhangyunkai <yxlc785@163.com>
2020-03-22 10:49:13 +0800

    ## 修改登录样式

--- bysj-framework/src/main/java/com/ruoyi/framework/config/ShiroConfig.java
+++ bysj-framework/src/main/java/com/ruoyi/framework/config/ShiroConfig.java
@@ -248,7 +248,7 @@ public class ShiroConfig
         filterChainDefinitionMap.put("/img/**", "anon");
         filterChainDefinitionMap.put("/ajax/**", "anon");
         filterChainDefinitionMap.put("/js/**", "anon");
-        filterChainDefinitionMap.put("/ruoyi/**", "anon");
+        filterChainDefinitionMap.put("/bysj/**", "anon");
         filterChainDefinitionMap.put("/captcha/captchaImage**", "anon");
         // 退出 logout地址，shiro去清除session
         filterChainDefinitionMap.put("/logout", "logout");
